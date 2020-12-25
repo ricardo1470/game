1d0a0257904cfb34fed4f62f341357c1cce4dd9b
Ricardo Alfonso Camayo Erazo <ricardo.alfonso.camayo@gmail.com>
2020-12-25 05:57:34 -0500

    create animation

---
+++ test_terrain/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float runSpeed = 7;
+    public float rotateSpeed = 250;
+
+    public Animator animator;
+
+    private float x, y;
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, x * Time.deltaTime * rotateSpeed, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * runSpeed);
+
+        animator.SetFloat("VelX", x);
+        animator.SetFloat("VelY", y);
+    }
+}
